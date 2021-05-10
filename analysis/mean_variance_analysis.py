a66adefe4dc6a93e7fc1bb5249bbe7bfeb08e70b
FernandoMarcon <fmarcon13@gmail.com>
2021-05-10 10:08:25 -0300

    measures in finance

--- analysis/mean_variance_analysis.py
+++ analysis/mean_variance_analysis.py
@@ -1,2 +1,5 @@
 # https://towardsdatascience.com/cryptocurrencies-the-new-frontier-part-1-940e787c7ab9
 # https://towardsdatascience.com/cryptocurrencies-the-new-frontier-part-2-7218c6a489f9
+
+# Measures in finance
+## return: rate of return, logarithmic return
