eea466177c2563aa86133a45ca94d15cd35435bc
FernandoMarcon <fmarcon13@gmail.com>
2021-05-25 18:14:13 -0300

    added data retrieval and init ml

---
+++ sentdex_tutorial/ml.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import pickle
+import os
+
+def process_data_for_labels(ticker):
+    hm_days = 7
+    df = pd.read_csv('data/sp500_joined_closes.csv', index_col=0)
+    tickers = df.columns.values.tolist()
+    df.fillna(0, inplace=True)
+
+    for i in range(1, hm_days+1):
+        df['{}_{}d'.format(ticker, i)] = (df[ticker].shift(-i) - df[ticker]) / df[ticker]
+
+    df.fillna(0, inplace=True)
+    return tickers, df
+
+# os.chdir('sentdex_tutorial/')
+process_data_for_labels('BMY')
+
+def buy_sell_hold(*args):
+    cols = [c for c in args]
+    requirement = 0.02
+    for col in cols:
+        if col > requirement:
+            return 1
+        if col < -requirement:
+            return -1
+    return 0
