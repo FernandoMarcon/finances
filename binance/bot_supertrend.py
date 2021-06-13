84c3ada742db540ca22e65cbc5dbc10c16d2f794
FernandoMarcon <fmarcon13@gmail.com>
2021-06-13 18:29:54 -0300

    added get data and viz

--- binance/bot_supertrend.py
+++ binance/bot_supertrend.py
@@ -7,6 +7,9 @@ import config
 # Authenticate
 client = Client(config.API_KEY, config.API_SECRET)
 
+# Settings
+symbol = 'BTCUSDT'
+
 # Account Balance
 def getBTC():
     balance = client.get_asset_balance('BTC')['free']
@@ -37,3 +40,37 @@ def sell(symbol = 'BTCUSDT'):
 #     price='0.00001')
 
 getUSDT()
+client.get_account()
+client.get_asset_details()['BTC']
+client.get_asset_dividend_history()
+client.get_trade_fee(symbol='BTCUSDT')
+fees = pd.DataFrame(client.get_trade_fee()).set_index('symbol')
+fees.loc['BTCUSDT']
+fees.head()
+interval = client.KLINE_INTERVAL_15MINUTE
+since = "1 day ago UTC"
+
+historical_data.head()
+
+def get_data(symbol, interval, since):
+    hist_df=pd.DataFrame(client.get_historical_klines(symbol,interval, since),
+                        columns = ['Open Time','Open','High','Low','Close','Volume','Close Time','Quote Asset Volume',
+                                            'Number of Trades','TB Base Volume','TB Quote Volume','Ignore'])
+
+    hist_df['Open Time'] = pd.to_datetime(hist_df['Open Time']/1000, unit = 's')
+    hist_df['Close Time'] = pd.to_datetime(hist_df['Close Time']/1000, unit = 's')
+    numeric_columns = ['Open','High','Low','Close','Volume','Quote Asset Volume','TB Base Volume','TB Quote Volume']
+    hist_df[numeric_columns] = hist_df[numeric_columns].apply(pd.to_numeric, axis=1)
+
+    return hist_df
+
+data = get_data(symbol, interval, since)
+data.head()
+data.tail()
+
+# Viz
+import mplfinance as mpf
+mpf.plot(data.set_index('Close Time'),
+        type = 'candle',style='charles',volume=True,
+        title = 'BTCUSDT Last 30 Min', mav = (10,20,30))
+        
