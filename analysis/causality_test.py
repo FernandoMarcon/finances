342362eb6d5fcdd261ae0effe05dadbeac7a8851
FernandoMarcon <fmarcon13@gmail.com>
2021-06-13 11:52:29 -0300

    binance EDA

--- analysis/causality_test.py
+++ analysis/causality_test.py
@@ -1,4 +1,9 @@
+# How to measure statistical causality: A Transfer Entropy Approach with Financial Applications
 # source: https://towardsdatascience.com/causality-931372313a1c
+# Open Quant Book: https://www.ebook.openquants.com/index.html
+# GOAL: Use statistics and information theory to uncover complex causal relationships from observational data
+# OBJECTIVES:
+# *
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,14 +20,19 @@ def x2(n):
     else:
         return 0
 
-print(x1(10))
-x2(10)
+def nonlinear_sys(n_max = 1000):
+    x1_vec = [x1(a) for a in range(0,n_max)]
+    x2_vec = [x2(a) for a in range(0,n_max)]
 
-n_max = 1000
-x1_vec = [x1(a) for a in range(0,n_max)]
-x2_vec = [x2(a) for a in range(0,n_max)]
+    plt.scatter(x1_vec, x2_vec, alpha = .5)
+    plt.title('2D Non-linear System')
+    plt.xlabel('x1')
+    plt.ylabel('x2')
 
-plt.scatter(x1_vec, x2_vec, alpha = .5)
-plt.title('2D Non-linear System')
-plt.xlabel('x1')
-plt.ylabel('x2')
+    return x1_vec, x2_vec
+
+x1_vec, x2_vec = nonlinear_sys()
+
+#### =============== Statistical Causality =============== ####
+# Granger causality:
+# X Granger-cause Y if the future realizations o Y is better explained using the past information from X and Y rather Y alone.
---
+++ analysis/get_live_youtube_signal.py
@@ -0,0 +1,5 @@
+# Capture live signals from youtube
+# YTB Live Signal: https://www.youtube.com/watch?v=whLEZUvQBVU
+# Source: https://stackoverflow.com/questions/43032163/how-to-read-youtube-live-stream-using-opencv-python
+
+import pafy
